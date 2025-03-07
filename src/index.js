b73516e13d9fe8c9f1cb013d9993526e3f454f95
Yurii <oniks9821@gmail.com>
2025-03-07 08:29:48 +0200

    0829

--- src/index.js
+++ src/index.js
@@ -3,16 +3,13 @@ import { setupServer } from './server.js';
 
 const startApp = async () => {
   try {
-    // Спочатку підключаємося до MongoDB
     await initMongoConnection();
 
-    // Якщо підключення успішне – запускаємо сервер
     setupServer();
   } catch (error) {
     console.error('Critical error during app startup:', error);
-    process.exit(1); // Завершуємо процес, якщо сталася критична помилка
+    process.exit(1);
   }
 };
 
-// Викликаємо функцію для запуску застосунку
 startApp();
